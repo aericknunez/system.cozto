f9cfd8a8794682d267fbc3ec15f30a86cbc7a442
aericknunez <42759691+aericknunez@users.noreply.github.com>
2020-09-10 16:10:52 -0600

    Update pesoBarcode.php

--- system/facturar/facturas/10/pesoBarcode.php
+++ system/facturar/facturas/10/pesoBarcode.php
@@ -66,13 +66,13 @@ if ($seslog->login_check() == TRUE) {
     <div class="text-center">
 
     <?php 
-    echo '<img src="../../../../application/common/Barcode.php?text='.$_REQUEST["cod"].'&size=35" alt="'.$producto.'" />';
+    echo '<img src="../../../../application/common/Barcode.php?text='.$_REQUEST["cod"].'&size=30" alt="'.$producto.'" />';
      ?>
     <br>
     <div style="margin-top: -8px; font-size: 12px;"><?php echo $producto ?></div>
     <div class="font-weight-bold" style="margin-top: -10px">Total: <?php echo Helpers::Dinero($total) ?></div>
         <div style="margin-top: -10px">Precio: <?php echo $pv ?> Peso: <?php echo $cant ?>
-            <br><small style="margin-top: -10px; font-size: 10px;">098908</small>
+            <br><small style="margin-top: -20px; font-size: 10px;">098908</small>
         </div>
     </div>
 </div>
