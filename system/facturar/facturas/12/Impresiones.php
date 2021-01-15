de599d138de8c219c773900cb52bc8ae3427ec2a
Erick Nunez <aerick.nunez@gmail.com>
2021-01-15 14:49:53 -0600

    Create Impresiones.php

---
+++ system/facturar/facturas/12/Impresiones.php
@@ -0,0 +1,93 @@
+ <?php  
+
+class Impresiones{
+    public function __construct() { 
+     } 
+
+
+  public function Ticket($efectivo, $numero){
+
+}
+
+
+
+
+
+
+
+ public function Factura($efectivo, $numero){
+
+}   /// termina FACTURA
+
+
+
+
+
+ public function CreditoFiscal($data){
+
+}
+
+
+
+
+
+
+
+
+
+
+ public function ImprimirAntes($efectivo, $numero, $cancelar){
+
+} /// TERMINA IMPRIMIR ANTES
+
+
+
+
+
+
+
+ public function Comanda(){
+
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ public function ReporteDiario($fecha){
+
+
+}   // termina reporte diario
+
+
+
+
+
+
+
+
+ public function AbrirCaja(){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+}// class
