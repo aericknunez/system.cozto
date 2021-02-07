6f53bbb2ff0ba770ee41e03e8377cc1b6e09211d
Erick Nunez <aerick.nunez@gmail.com>
2021-02-07 13:37:49 -0600

    Update Impresiones.php

--- system/facturar/facturas/23/Impresiones.php
+++ system/facturar/facturas/23/Impresiones.php
@@ -186,9 +186,9 @@ $oi=80;
 //// comienza la factura
 
 $oi=$oi+$n1;
-printer_draw_text($handle, date("d"), 35, $oi);
-printer_draw_text($handle, date("m"), 120, $oi);
-printer_draw_text($handle, substr(date("Y"), -1), 180, $oi);
+printer_draw_text($handle, date("d"), 430, $oi);
+printer_draw_text($handle, date("m"), 490, $oi);
+printer_draw_text($handle, substr(date("Y"), -1), 590, $oi);
 
 
 
