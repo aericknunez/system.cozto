6cc077429f6102a7c0bf441c5580001839d280cb
Erick Nunez <aerick.nunez@gmail.com>
2021-02-12 09:33:49 -0600

    Update FacturarWeb.php

--- system/facturar/FacturarWeb.php
+++ system/facturar/FacturarWeb.php
@@ -22,6 +22,63 @@ foreach ($x as $z) {
 
 $parametros["productos"] = $productos;
 $parametros["tipoticket"] = $_SESSION["tipoticket"];
+$parametros["identidad"] = $_SESSION["td"];
+$parametros["config_imp"] = $_SESSION['config_imp'];
+
+
+
+if($_SESSION["tipoticket"] == 2){
+
+if ($r = $db->select("cliente", "ticket_cliente", "WHERE factura = '$numero' and tx = " . $_SESSION["tx"] . " and td = " .  $_SESSION["td"])) { 
+$hashcliente = $r["cliente"];
+} unset($r);  
+
+
+
+if ($r = $db->select("nombre, documento, direccion", "clientes", "WHERE hash = '$hashcliente' and td = " .  $_SESSION["td"])) { 
+$parametros["nombre"] = $r["nombre"];
+$parametros["docimento"] = $r["documento"];
+$parametros["direccion"] = $r["direccion"];
+} unset($r);  
+
+
+
+if ($sx = $db->select("sum(stotal), sum(imp), sum(total)", "ticket", "WHERE num_fac = '".$parametros["num_fac"]."' and tx = ".$_SESSION["tx"]." and td = ".$_SESSION["td"]." and tipo = ".$_SESSION["tipoticket"]."")) { 
+       $parametros["stotal"]=$sx["sum(stotal)"];
+       $parametros["imp"]=$sx["sum(imp)"];
+       $parametros["total"]=$sx["sum(total)"];
+    } unset($sx); 
+ 
+
+}
+
+
+
+if($_SESSION["tipoticket"] == 3){
+
+    if ($r = $db->select("documento", "facturar_documento_factura", "WHERE factura = '$numero' and tx = " . $_SESSION["tx"] . " and td = " .  $_SESSION["td"])) { 
+        $documento = $r["documento"];
+    } unset($r);  
+
+    if ($r = $db->select("cliente, giro, registro, direccion, departamento", "facturar_documento", "WHERE documento = '$documento' and td = " .  $_SESSION["td"])) { 
+        $parametros["cliente"] = $r["cliente"];
+        $parametros["giro"] = $r["giro"];
+        $parametros["registro"] = $r["registro"];
+        $parametros["direccion"] = $r["direccion"];
+        $parametros["departamento"] = $r["departamento"];
+    } unset($r);  
+
+if ($sx = $db->select("sum(stotal), sum(imp), sum(total)", "ticket", "WHERE num_fac = '".$parametros["num_fac"]."' and tx = ".$_SESSION["tx"]." and td = ".$_SESSION["td"]." and tipo = ".$_SESSION["tipoticket"]."")) { 
+       $parametros["stotal"]=$sx["sum(stotal)"];
+       $parametros["imp"]=$sx["sum(imp)"];
+       $parametros["total"]=$sx["sum(total)"];
+    } unset($sx); 
+ 
+}
+
+
+
+
 
 echo json_encode($parametros);
 		
