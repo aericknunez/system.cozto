58eb344bf137d53a0bcbdee710a09474ba5c1b98
aericknunez <aerick.nunez@gmail.com>
2021-02-23 20:18:59 -0600

    Update variables_db.php

--- application/includes/variables_db.php
+++ application/includes/variables_db.php
@@ -20,10 +20,10 @@ define("PASSWORD", "caca007125-"); 	// The database password.
 
 } else if(Helpers::AmazonServer() == TRUE) {
 
-define("HOST", "198.27.68.160"); 			//35.225.56.157 The host you want to connect to. 
-define("USER", "superpol_erick"); 			// The database username. 
+define("HOST", "localhost"); 			//35.225.56.157 The host you want to connect to. 
+define("USER", "root"); 			// The database username. 
 define("PASSWORD", "caca007125-"); 	// The database password. 
-define("DATABASE", "superpol_demo_cozto"); 
+define("DATABASE", "cozto_ventas"); 
 define("XSERV", "//http://3.18.81.185/cozto/");	
 
 } else {
