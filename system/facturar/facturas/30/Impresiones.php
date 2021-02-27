ee9d756b6bcf478824f7445c8e859d90cf81126a
aericknunez <aerick.nunez@gmail.com>
2021-02-26 20:46:55 -0600

    Impresiones de Nueva

---
+++ system/facturar/facturas/30/Impresiones.php
@@ -0,0 +1,169 @@
+ <?php  
+use Mike42\Escpos\EscposImage;
+use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
+use Mike42\Escpos\Printer;
+
+
+
+class Impresiones{
+    public function __construct() { 
+     } 
+
+
+
+ public function Ticket($efectivo, $numero){
+
+
+}
+
+
+
+
+
+
+
+ public function Factura($efectivo, $numero){
+
+}   /// termina FACTURA
+
+
+
+
+
+ public function CreditoFiscal($efectivo, $numero){
+
+}
+
+
+
+
+
+
+
+
+
+ public function Ninguno(){
+
+
+}   /// termina /.;ninguno
+
+
+
+
+
+
+ public function ImprimirAntes($efectivo, $numero, $cancelar){
+  $db = new dbConn();
+
+
+} /// TERMINA IMPRIMIR ANTES
+
+
+
+
+
+
+
+ public function Comanda(){
+
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ public function ReporteDiario($fecha){
+
+
+}   // termina reporte diario
+
+
+
+
+
+
+
+
+ public function AbrirCaja(){
+ // $print
+
+}
+
+
+
+
+
+
+
+ public function Barcode($numero){
+ 
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ public function Item($cant,  $name = '', $price = '', $total = '', $dollarSign = false)
+    {
+        $rightCols = 8;
+        $leftCols = 38;
+        if ($dollarSign) {
+            $leftCols = $leftCols / 2 - $rightCols / 2;
+        }
+        $left = str_pad($cant . " " . $name, $leftCols) ;
+        
+        $sign = ($dollarSign ? '$ ' : '');
+
+        $total = str_pad($sign . $total, $rightCols, ' ', STR_PAD_LEFT);
+        $right = str_pad($sign . $price, $rightCols, ' ', STR_PAD_LEFT);
+        return "$left$right$total\n";
+    }
+
+
+
+ public function DosCol($izquierda = '', $iz, $derecha = '', $der)
+    {
+        $left = str_pad($izquierda, $iz, ' ', STR_PAD_LEFT) ;      
+        $right = str_pad($derecha, $der, ' ', STR_PAD_LEFT);
+        return "$left$right\n";
+    }
+
+
+
+
+
+
+
+
+
+
+
+}// class
