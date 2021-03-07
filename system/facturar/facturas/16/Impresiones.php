7fb409154e6516bda38eff6c50a9f2ac6d380333
aericknunez <aerick.nunez@gmail.com>
2021-03-07 16:39:51 -0600

    Update Impresiones.php

--- system/facturar/facturas/16/Impresiones.php
+++ system/facturar/facturas/16/Impresiones.php
@@ -281,8 +281,8 @@ $printer -> setBarcodeHeight(100);
 $printer->setBarcodeTextPosition(Printer::BARCODE_TEXT_BELOW);
     $printer -> barcode($a, Printer::BARCODE_CODE128);
 
-    if ($r = $db->select("descripcion", "producto", "WHERE cod = '$numero' and td = ".$_SESSION["td"]."")) { 
-    $pro = $r["descripcion"];
+if ($r = $db->select("descripcion", "producto", "WHERE cod = '$numero' and td = ".$_SESSION["td"]."")) { 
+$pro = $r["descripcion"];
 } unset($r);  
 
 $printer -> feed(1);
