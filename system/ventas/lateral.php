7248738dac9a4aea0eeb1be5bb612f8ebe5476a3
Erick Nunez <aerick.nunez@gmail.com>
2021-05-11 18:00:21 -0600

    Push

--- system/ventas/lateral.php
+++ system/ventas/lateral.php
@@ -2,6 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 if(!isset($_GET["modal"])){
+	
 	if($venta == TRUE){
 		include_once 'system/ventas/Laterales.php';
 		$lateral = new Laterales(); 
@@ -16,6 +17,7 @@ if(!isset($_GET["modal"])){
 	} else {
 		echo '<div class="text-center"><img src="assets/img/logo/'.$_SESSION['config_imagen'].'" class="img-fluid responsive" alt="Responsive image"></div>';
 	}
+
 } 
 ?>
 
