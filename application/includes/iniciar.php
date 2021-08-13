0dade48b6aba9bb6336314f1489dc55ffd33c6a3
aericknunez <aerick.nunez@gmail.com>
2021-08-13 10:11:09 -0600

    Update iniciar.php

--- application/includes/iniciar.php
+++ application/includes/iniciar.php
@@ -58,6 +58,10 @@ $_SESSION["ver_avatar"] = NULL;
         }
 
 
+if ($_SESSION["td"] == 46) {
+    AgregaParaTaller();
+}
+
 
 // agrego el tipo de ticket predeterminado
 if ($r = $db->select("predeterminado", "facturar_opciones", "WHERE td = ".$_SESSION["td"]."")) { 
@@ -170,6 +174,38 @@ $archivos = glob("../../sync/database/*.sql");
         } 
     } // termina busqueda de archivos en la carpeta
 } // termina Import
+
+
+
+
+function AgregaParaTaller(){
+    $db = new dbConn();
+
+    $a = $db->query("SELECT cod FROM producto WHERE td = " . $_SESSION["td"]);
+    foreach ($a as $b) {
+
+        if ($r = $db->select("medida", "taller_medida", "WHERE td = " . $_SESSION["td"])) { 
+           $medida = $r["medida"];
+        }
+
+        if ($medida == NULL) {
+
+            $datos = array();
+            $datos["medida"] = NULL;
+            $datos["producto"] = $b["cod"];
+            $datos["hash"] = Helpers::HashId();
+            $datos["time"] = Helpers::TimeId();
+            $datos["td"] = $_SESSION["td"];
+            $db->insert("taller_medida", $datos);
+            
+        }
+
+
+
+
+    } $a->close();
+
+}
 
 
 
