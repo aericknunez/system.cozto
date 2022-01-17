d1a6c8c75ad6737e87eeded49057c6550d994f7a
aericknunez <aerick.nunez@gmail.com>
2022-01-17 13:28:18 -0600

    Cot

--- assets/js/printThis_print.js
+++ assets/js/printThis_print.js
@@ -3,13 +3,13 @@ $(document).ready(function(){
 
     $('#imprimir').on("click", function () {
       $('#vista').printThis({
-        importCSS: false,
+        importCSS: true,
         loadCSS: [
         "http://localhost/cozto/assets/css/font-awesome-582.css",
         "http://localhost/cozto/assets/css/bootstrap.min.css", 
         "http://localhost/cozto/assets/css/mdb.min.css",
         "http://localhost/cozto/assets/css/galeria.css"],
-        removeScripts: true,
+         removeScripts: false,
          base: false
       });
     });
