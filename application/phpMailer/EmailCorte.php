3c48c531a3e9b366c6087df212939553851aaa8b
aericknunez <aerick.nunez@gmail.com>
2022-06-04 08:44:26 -0600

    Variables

--- application/phpMailer/EmailCorte.php
+++ application/phpMailer/EmailCorte.php
@@ -687,10 +687,10 @@ $r = $db->select("*", "corte_diario", "WHERE edo = 2 and user = '".$_SESSION["us
     //Server settings
     $mail->SMTPDebug = false;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.ionos.com';                    // Set the SMTP server to send through
+    $mail->Host       = Encrypt::decrypt('Q0l3ZzlTWWJ5TmNZM2l2TnpxQ2xSUT09', 'https://hibridosv.com');                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'no_reply@hibridosv.com';                     // SMTP username
-    $mail->Password   = 'Caca007125-';                                // SMTP password
+    $mail->Password   = Encrypt::decrypt('RnhOK3hpcWU2UnpBN2Foc2FKTU93Zz09', 'https://hibridosv.com');                                // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
