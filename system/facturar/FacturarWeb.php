17d7b497a83c3624fcfcfdc15cdd8a19af12bb72
aericknunez <42759691+aericknunez@users.noreply.github.com>
2023-11-13 12:40:54 -0600

    Ticket 0

--- system/facturar/FacturarWeb.php
+++ system/facturar/FacturarWeb.php
@@ -27,6 +27,38 @@ $parametros["config_imp"] = $_SESSION['config_imp'];
 $parametros["cajero"] = $_SESSION['nombre'];
 
 
+if($_SESSION["tipoticket"] == 0){
+
+    if ($r = $db->select("cliente", "ticket_cliente", "WHERE factura = '".$parametros["num_fac"]."' and tx = " . $_SESSION["tx"] . " and td = " .  $_SESSION["td"])) { 
+    $hashcliente = $r["cliente"];
+    } unset($r);  
+    
+    
+    if ($r = $db->select("nombre, documento, direccion", "clientes", "WHERE hash = '".$hashcliente."' and td = " .  $_SESSION["td"])) { 
+    $parametros["nombre"] = $r["nombre"];
+    $parametros["documento"] = $r["documento"];
+    $parametros["direccion"] = $r["direccion"];
+    } unset($r);  
+    
+    
+    if ($sx = $db->select("sum(stotal), sum(imp), sum(total)", "ticket", "WHERE num_fac = '".$parametros["num_fac"]."' and tx = ".$_SESSION["tx"]." and td = ".$_SESSION["td"]." and tipo = ".$_SESSION["tipoticket"]."")) { 
+           $parametros["stotal"]=$sx["sum(stotal)"];
+           $parametros["imp"]=$sx["sum(imp)"];
+           $parametros["total"]=$sx["sum(total)"];
+        } unset($sx); 
+     
+    
+    /// sistema - cliente
+    $parametros["c_cliente"] = $_SESSION["config_cliente"];
+    $parametros["c_propietario"] = $_SESSION["config_propietario"];
+    $parametros["c_telefono"] = $_SESSION["config_telefono"];
+    $parametros["c_direccion"] = $_SESSION["config_direccion"];
+    $parametros["c_giro"] = $_SESSION["config_giro"];
+    
+    
+    }
+
+
 if($_SESSION["tipoticket"] == 1){
 
 if ($r = $db->select("cliente", "ticket_cliente", "WHERE factura = '".$parametros["num_fac"]."' and tx = " . $_SESSION["tx"] . " and td = " .  $_SESSION["td"])) { 
