7cb19313d5d4dfa55b0d8404149cd3bf82e4b1a5
Erick Nuñez <42759691+aericknunez@users.noreply.github.com>
2024-07-10 08:08:02 -0600

    Update variables_db.php

--- application/includes/variables_db.php
+++ application/includes/variables_db.php
@@ -2,22 +2,22 @@
 date_default_timezone_set('America/El_Salvador');
 
 
-define("HOST", "localhost");	 
-define("USER", "sistxtyv_erick"); 					
-define("PASSWORD", Encrypt::decrypt('ZmdEV09vNnBIU2pOTVVWczBMR2dnZz09', 'https://hibridosv.com')); 			
-define("DATABASE", "sistxtyv_cozto");
-define("XSERV", "http://sistema-sv.pro/");
-define("TYPE", "OnLine");			
-
-
-// define("HOST", "127.0.0.1");	 
-// define("USER", Encrypt::decrypt('SmhpQU5jT0FGUkdSTXZRZUpnM1BCUT09', 'https://hibridosv.com')); 					
-// define("PASSWORD", Encrypt::decrypt('eTB5TmNOcHBMaFJkemRGSW1ISmdUd0ZRTHdyNWhLVVd2cklIRk9kZmRoOD0=', 'https://hibridosv.com')); 			
-// define("DATABASE", Encrypt::decrypt('elM4cFozL1pMN2dFWWxRMXk2VGIvUT09', 'https://hibridosv.com'));
-// define("XSERV", "https://sistema.pizto.com/");
+// define("HOST", "localhost");	 
+// define("USER", "sistxtyv_erick"); 					
+// define("PASSWORD", Encrypt::decrypt('ZmdEV09vNnBIU2pOTVVWczBMR2dnZz09', 'https://hibridosv.com')); 			
+// define("DATABASE", "sistxtyv_cozto");
+// define("XSERV", "http://sistema-sv.pro/");
 // define("TYPE", "OnLine");			
 
-define("URL_TRANSFERENCIA", "http://sistema-sv.pro/coztoapi/");  
+
+define("HOST", "127.0.0.1");	 
+define("USER", Encrypt::decrypt('SmhpQU5jT0FGUkdSTXZRZUpnM1BCUT09', 'https://hibridosv.com')); 					
+define("PASSWORD", Encrypt::decrypt('eTB5TmNOcHBMaFJkemRGSW1ISmdUd0ZRTHdyNWhLVVd2cklIRk9kZmRoOD0=', 'https://hibridosv.com')); 			
+define("DATABASE", Encrypt::decrypt('elM4cFozL1pMN2dFWWxRMXk2VGIvUT09', 'https://hibridosv.com'));
+define("XSERV", "https://sistema.pizto.com/");
+define("TYPE", "OnLine");			
+
+define("URL_TRANSFERENCIA", "https://sistema.pizto.com/coztoapi/");  
 
 
 define("CAN_REGISTER", "any");
@@ -25,8 +25,8 @@ define("DEFAULT_ROLE", "member");
 define("SECURE", FALSE);    						// For development purposes only!!!!
 
 // para el sistema
-define("BASE_URL", "http://sistema-sv.pro/");
-define("BASEPATH", "http://sistema-sv.pro/");	
+define("BASE_URL", "https://sistema.pizto.com/");
+define("BASEPATH", "https://sistema.pizto.com/");	
 
 define("VERSION", 01.04);	
 ?>
